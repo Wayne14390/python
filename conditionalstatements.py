02b8c0425cd67d32bbf43de2b062f7897fd893de
Wayne14390 <wayinsavage@gmail.com>
2025-02-06 16:38:13 +0300

    third commit

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -1,21 +1,36 @@
 # if.....else...
 # loops
-votes = 0
+votes = 10000
 if votes > 10000:
     print(f"You got {votes} and therefore you win!")
 else:
     print(f"You got {votes} and therefore you fail!")
 # input from user
-age = int(input("What is your age? "))
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not 18+.")
+# age = int(input("What is your age? "))
+# if age >= 18:
+#     print("You are eligible to vote.")
+# else:
+#     print("You are not 18+.")
+#
+# number = int(input("Enter your number:"))
+# if number > 0:
+#     print("The number is positive.")
+# elif number < 0:
+#     print("The number is negative.")
+# else:
+#     print("The number is zero.")
 
-number = int(input("Enter your number:"))
-if number > 0:
-    print("The number is positive.")
-elif number < 0:
-    print("The number is negative.")
+marks = 100
+if marks > 80 and marks <= 100:
+    print(f"You got {marks} and  you have grade A")
+elif marks > 70 and marks <= 80:
+    print(f"You got {marks} and  you have grade B")
+elif marks > 60 and marks <= 70:
+    print(f"You got {marks} and  you have grade C")
+elif marks > 40 and marks <= 60:
+    print(f"You got {marks} and  you have grade D")
+elif marks <40 and marks >= 0:
+    print(f"You got {marks} and  you have grade E")
 else:
-    print("The number is zero.")
+    print('Please enter a number between 0 and 100')
+
--- test.py
+++ test.py
@@ -1,16 +1,54 @@
-# input from user
-secret_code = float(input("Enter the secret code: "))
-print(f"The secret code is {secret_code>50 and secret_code<100}")
-# dictionary
-book = {
-    'title' : 'Samaritan',
-    'author' : 'Wayne',
-     'publisher' : 'klb',
-     'key' : '1232'
-}
-print(book)
+# # input from user
+# secret_code = float(input("Enter the secret code: "))
+# print(f"The secret code is {secret_code>50 and secret_code<100}")
+# # dictionary
+# book = {
+#     'title' : 'Samaritan',
+#     'author' : 'Wayne',
+#      'publisher' : 'klb',
+#      'key' : '1232'
+# }
+# print(book)
+#
+# if 'key' in book:
+#     print('key is present')
+# else :
+#     print('key is not present')
+age = int(input("Enter your age: "))
+print(age)
+if age > 0 and age <= 2:
+    print('You are a baby.')
+elif age > 2 and age <= 4:
+    print('You are a toddler.')
+elif age > 4 and age <= 13 :
+    print('You are a child.')
+elif age > 13 and age <= 20 :
+    print('You are a teenager.')
+elif age > 20 and age <= 35 :
+    print('You are a young adult.')
+elif age > 35 and age <= 60 :
+    print('You are a middle-aged adult.')
+else:
+    print('You are a senior citizen.')
+print('Age is just a number, enjoy life!')
 
-if 'key' in book:
-    print('key is present')
-else :
-    print('key is not present')
+current_temperature = float(input("Enter your current temperature: "))
+print(f'{current_temperature} degrees celsius')
+if current_temperature > 30:
+    print('Its a hot day.')
+elif current_temperature <= 30 and current_temperature > 20:
+    print('Its a warm day.')
+else:
+    print('Its a cold day.')
+
+withdrawal_amount = float(input("Enter your withdrawal amount: "))
+if withdrawal_amount > 10000:
+    withdrawal_amount = withdrawal_amount + (withdrawal_amount *0.1)
+    print(f'Withdrawal amount: {withdrawal_amount}.')
+elif withdrawal_amount > 5000:
+    withdrawal_amount = withdrawal_amount + (withdrawal_amount *0.05)
+    print(f'Withdrawal amount: {withdrawal_amount}.')
+else:
+    print(f'Withdrawal amount: {withdrawal_amount}.')
+
+
